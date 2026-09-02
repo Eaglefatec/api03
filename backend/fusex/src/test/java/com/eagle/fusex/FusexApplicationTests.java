69834d169cb9674c3670fdf727168d8b5b5bfe5c
Thayssa Andrade <andradethayssa03@gmail.com>
2026-09-01 22:12:38 -0300

    chore: criando estrutura spring e docker compose

---
+++ backend/fusex/src/test/java/com/eagle/fusex/FusexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.fusex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
