910bace0da17e05480c62020d8d513feeffae34a
cefasbreno <brenocefas@outlook.com>
2026-09-24 00:49:27 -0300

    fix: correção real do V6

--- backend/fusex/src/main/resources/db/migration/V6__create_ocs_proc_juncao_schema.sql
+++ backend/fusex/src/main/resources/db/migration/V6__create_ocs_proc_juncao_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ocs_procedimento (
-    ocs_id BIGINT,
-    proc_codigo_dgp BIGINT,
-    PRIMARY KEY (ocs_id, proc_id),
+    ocs_id BIGINT NOT NULL,
+    proc_codigo_dgp VARCHAR(100) NOT NULL,
+    PRIMARY KEY (ocs_id, proc_codigo_dgp),
     FOREIGN KEY (ocs_id) REFERENCES ocs(ocs_id),
     FOREIGN KEY (proc_codigo_dgp) REFERENCES procedimento(proc_codigo_dgp)
 );
