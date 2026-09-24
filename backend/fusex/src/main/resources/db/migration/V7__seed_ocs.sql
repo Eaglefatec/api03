32427ad0e598079addd63b80ce7263f45637b269
cefasbreno <brenocefas@outlook.com>
2026-09-24 11:16:52 -0300

    test: cria ocs de teste

---
+++ backend/fusex/src/main/resources/db/migration/V7__seed_ocs.sql
@@ -0,0 +1,5 @@
+INSERT INTO ocs (ocs_nome, ocs_inscricao_federal, ocs_endereco, ocs_endereco_numero, ocs_endereco_bairro, ocs_endereco_cidade,
+    ocs_endereco_uf, ocs_endereco_cep, ocs_contato_nome, ocs_contato_telefone) VALUES ('Fisio Vip Clin',
+        '07202407000140', 'Rua Cel Joao Dias Guimaraes', '78',
+            'Centro', 'Cacapava', 'SP', '12281350',
+                'Maria', '1236528026');
